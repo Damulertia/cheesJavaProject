fbe4cbea806e932e2e8b76ce9ca1ea666e3d6372
Everton Rodrigo Tefili <tefili1978@yahoo.com.br>
2021-03-09 16:08:42 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Initial commit chess project");
+	Position pos = new Position(3, 5);
+	System.out.println(pos);
 
 	}
 
