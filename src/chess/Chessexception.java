95fc39d706954d16ef4451d165fca9ffa09a487a
Everton Rodrigo Tefili <tefili1978@yahoo.com.br>
2021-03-09 21:37:28 -0300

    Chess exception and Chess position

---
+++ src/chess/Chessexception.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class Chessexception extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public Chessexception(String msg) {
+		super(msg);
+	}
+	
+
+}
